d963111293fff7de0b07046090efc56cc5ac4adf
juliwind <julianwindheuser@gmail.com>
2021-10-23 18:45:16 +0200

    2 tasks finished

--- runde1/treffsicherheit/main.py
+++ runde1/treffsicherheit/main.py
@@ -1,16 +1,61 @@
+import math
+
+
 def readFile():
     info = []
     with open("file.txt", 'r') as file:
         for line in file:
             for word in line.split():
                 info.append(word)
-
+    # planner = [[0 for l in range(int(info[0]))]for c in range(int(info[1]))]
     lines = int(info[0])
     columns = int(info[1])
-    planner = [[info[l*columns+c+2]
-                for c in range(columns)] for l in range(lines)]
+    planner = [[info[l*columns+c+2] for l in range(lines)]
+               for c in range(columns)]
 
-    print(planner)
+    return(planner)
 
 
-readFile()
+planner = readFile()
+print(planner)
+# planner[T][P]
+
+
+def isBestDate(t, p):
+    if int(planner[t][p]) == 0:
+        return True
+    else:
+        return False
+#        for i in t:
+#            if int(t) > int(t):
+#                return False
+#        return True
+#    return False
+
+
+bestDate = None
+final_adjustments = math.inf
+for t in range(len(planner)):   # geht alle termine durch
+    adjustments = 0
+    for p in range(len(planner[t])):     # geht alle personen durch
+        if isBestDate(t, p) == False:
+            # p = 0
+            adjustments += 1
+    if adjustments < final_adjustments:
+        final_adjustments = adjustments
+        bestDate = t
+
+
+print("bestDate", bestDate, "number of adjustments", final_adjustments)
+
+
+# [['0', '1', '2', '2', '0', '1'], TERMIN 1
+# ['0', '0', '2', '1', '1', '2'], TERMIN 2
+# ['0', '0', '2', '1', '2', '1'], TERMIN 3
+# ['0', '1', '1', '1', '2', '2'], TERMIN 4
+# ['0', '1', '2', '2', '1', '0'], TERMIN 5
+# ['0', '0', '2', '1', '0', '1'], TERMIN 6
+# ['0', '0', '2', '2', '0', '1']] TERMIN 7
+
+#   1    2    3    4    5    6
+# PERSON PERSON PERSON PERSON PERSON PERSON
--- runde1/zum_winde_verweht/main.py
+++ runde1/zum_winde_verweht/main.py
@@ -1,24 +1,43 @@
-import csv
 import math
+
+buildings = []
+windwheels = []
+
+
+def readFile():
+    info = []
+    with open("file.txt", 'r') as file:
+        for line in file:
+            for word in line.split():
+                info.append(int(word))
+    n = info[0]
+    m = info[1]
+    for i in range(2, n*2+2, 2):
+        new_building = []
+        new_building.append(info[i])
+        new_building.append(info[i+1])
+        buildings.append(new_building)
+
+    for i in range(n*2+2, 2*(n+m)+2, 2):
+        new_windwheel = []
+        new_windwheel.append(info[i])
+        new_windwheel.append(info[i+1])
+        windwheels.append(new_windwheel)
+
+
+readFile()
+
 
 def distance(x1, y1, x2, y2):
     return math.sqrt((x1-x2)**2 + (y1-y2)**2)
 
-with open("gebaeude.csv", newline='') as csvfile:
-    buildings = list(csv.reader(csvfile))
 
-with open("windraeder.csv", newline='') as csvfile:
-     windWheels = list(csv.reader(csvfile))
-
-
-
-for w in windWheels:
+for w in windwheels:
     min_dist = math.inf
     for b in buildings:
-        curr_dist = distance(float(w[1]), float(w[2]), float(b[1]), float(b[2]))
+        curr_dist = distance(float(w[0]), float(
+            w[1]), float(b[0]), float(b[1]))
         if (curr_dist < min_dist):
             min_dist = curr_dist
     w.append(min_dist / 10.0)
-print(windWheels)
-
-
+print(windwheels)
