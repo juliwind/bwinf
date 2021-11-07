103f5937984996c99ac8b707dbe478cabb37c081
juliwind <julianwindheuser@gmail.com>
2021-11-07 20:15:15 +0100

    finished

--- runde1/treffsicherheit/main.py
+++ runde1/treffsicherheit/main.py
@@ -7,7 +7,6 @@ def readFile():
         for line in file:
             for word in line.split():
                 info.append(word)
-    # planner = [[0 for l in range(int(info[0]))]for c in range(int(info[1]))]
     lines = int(info[0])
     columns = int(info[1])
     planner = [[info[l*columns+c+2] for l in range(lines)]
@@ -18,44 +17,24 @@ def readFile():
 
 planner = readFile()
 print(planner)
-# planner[T][P]
 
 
 def isBestDate(t, p):
-    if int(planner[t][p]) == 0:
-        return True
-    else:
-        return False
-#        for i in t:
-#            if int(t) > int(t):
-#                return False
-#        return True
-#    return False
+    for i in range(len(planner)):
+        if int(planner[t][p]) > int(planner[i][p]):
+            return False
+    return True
 
 
 bestDate = None
 final_adjustments = math.inf
-for t in range(len(planner)):   # geht alle termine durch
+for t in range(len(planner)):
     adjustments = 0
-    for p in range(len(planner[t])):     # geht alle personen durch
+    for p in range(len(planner[t])):
         if isBestDate(t, p) == False:
-            # p = 0
             adjustments += 1
     if adjustments < final_adjustments:
         final_adjustments = adjustments
         bestDate = t
 
-
-print("bestDate", bestDate, "number of adjustments", final_adjustments)
-
-
-# [['0', '1', '2', '2', '0', '1'], TERMIN 1
-# ['0', '0', '2', '1', '1', '2'], TERMIN 2
-# ['0', '0', '2', '1', '2', '1'], TERMIN 3
-# ['0', '1', '1', '1', '2', '2'], TERMIN 4
-# ['0', '1', '2', '2', '1', '0'], TERMIN 5
-# ['0', '0', '2', '1', '0', '1'], TERMIN 6
-# ['0', '0', '2', '2', '0', '1']] TERMIN 7
-
-#   1    2    3    4    5    6
-# PERSON PERSON PERSON PERSON PERSON PERSON
+print("bestDate", bestDate + 1, "number of adjustments", final_adjustments)
